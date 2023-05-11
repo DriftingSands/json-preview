2f23daba438a8731d2d92340c4ef09f99555a14f
Patrick Zimmermann <patrickmzimmermann@gmail.com>
2023-05-11 16:47:43 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react"
+import './App.css'
+import CodeMirror from '@uiw/react-codemirror'
+import { json, jsonParseLinter } from "@codemirror/lang-json";
+import { linter, lintGutter } from "@codemirror/lint";
+
+function App() {
+  const [data, setData] = useState({status: "Waiting for data...", message: "please create some data using the CF-editor."})
+  useEffect(() => {
+    const dataHandler = (event) => {
+      if (event.data.type !== "setCfData") {
+        return;
+      }
+      setData(event.data.payload.data);
+    };
+
+    window.addEventListener("message", dataHandler);
+    return () => window.removeEventListener("message", dataHandler);
+  }, [])
+  
+
+  return (
+    <main>
+      <CodeMirror
+        value={JSON.stringify(data, null, 2)}
+        height="100%"
+        theme={"dark"}
+        editable={false}
+        extensions={
+          [json(), linter(jsonParseLinter()), lintGutter()]
+        }
+      />
+    </main>
+  )
+}
+
+export default App
